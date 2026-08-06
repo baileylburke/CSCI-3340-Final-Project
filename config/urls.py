6d2f98ad05af2226960c94839339a2464937e4f6
jlopez9345 <jl750950@gmail.com>
2026-08-06 18:05:38 -0500

    Front end more presentable and made login and sign up buttons function and redirect you

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.core.urls")),
     # Add as each app grows a urls.py:
+    path("", include("apps.accounts.urls")),
     # path("projects/", include("apps.projects.urls")),
     # path("chat/", include("apps.chat.urls")),
     # path("schedule/", include("apps.schedule.urls")),
